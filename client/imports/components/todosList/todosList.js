ed69e4cf98c9c0568ef4bf497b777b7f37e895aa
Steven Medina <stevenmedina.co@gmail.com>
2016-06-16 12:08:17 -0500

    Todo App Meteor ;)

---
+++ client/imports/components/todosList/todosList.js
@@ -0,0 +1,23 @@
+import angular from 'angular';
+import angularMeteor from 'angular-meteor';
+import template from './todosList.html';
+
+class TodosListCtrl {
+    constructor( $scope ) {
+        $scope.viewModel(this);
+
+        this.helpers({
+            tasks() {
+                return Tasks.find({});
+            }
+        })
+    }
+}
+
+export default angular.module( 'todosList', [
+    angularMeteor
+])
+    .component('todosList', {
+        templateUrl: 'imports/components/todosList/todosList.html',
+        controller: ['$scope', TodosListCtrl]
+    });
---
+++ imports/components/todosList/todosList.js
@@ -0,0 +1,49 @@
+import angular from 'angular';
+import angularMeteor from 'angular-meteor';
+import template from './todosList.html';
+import { Tasks } from '../../api/tasks.js';
+
+class TodosListCtrl {
+  constructor($scope) {
+    $scope.viewModel(this);
+
+    this.helpers({
+      tasks() {
+        return Tasks.find({}, {
+            sort: {
+              createdAt: -1
+            }
+        });
+      }
+    })
+  }
+
+  addTask(newTask) {
+      Tasks.insert({
+          text: newTask,
+          createdAt: new Date
+      });
+
+      this.newTask = '';
+  }
+
+  setChecked(task) {
+      Tasks.update(task._id, {
+          $set: {
+            checked: !task.checked
+          },
+      });
+  }
+
+  removeTask(task) {
+      Tasks.remove(task._id);
+  }
+}
+
+export default angular.module('todosList', [
+  angularMeteor
+])
+  .component('todosList', {
+    templateUrl: 'imports/components/todosList/todosList.html',
+    controller: TodosListCtrl
+  });
